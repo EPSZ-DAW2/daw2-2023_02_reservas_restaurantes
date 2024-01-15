53f38e9e1d0bf246ff10a53fa2afdc8790566bbc
MikelInesta <mikel_inesta@hotmail.com>
2024-01-15 17:14:59 +0100

    Vista principal "inicio"

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -3,6 +3,8 @@
 /** @var yii\web\View $this */
 use \app\widgets\CarruselRestaurante;
 use \app\widgets\BarraBusqueda;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = 'La Cuchara - Inicio';
 ?>
@@ -25,6 +27,13 @@ $this->title = 'La Cuchara - Inicio';
 
         <hr>
         <!-- Registra tu restaurante -->
+
+        <div style="background-image: url('<?= Yii::getAlias('@web') ?>/multimedia/eresPropietario.jpg'); background-size: cover; height: 250px;">
+            <div class="text-center" style="background-color: rgba(0, 0, 0, 0.5); padding: 20px; height: 100%;">
+                <h1 class="text-white">¿Quieres registrar tu restaurante?</h1>
+                <?= Html::a('Registrate como propietario', [Url::to('registrar-propietario')], ['class' => 'btn btn-primary']) ?>
+            </div>
+        </div>
 
     </div>
 </div>
