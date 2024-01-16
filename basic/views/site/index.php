173234f8ced55453a58aad8fd38b5e27349b7cf5
MikelInesta <mikel_inesta@hotmail.com>
2024-01-16 22:11:53 +0100

    integracion primera entrega

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -32,7 +32,7 @@ $this->title = 'La Cuchara - Inicio';
         <div style="background-image: url('<?= Yii::getAlias('@web') ?>/multimedia/eresPropietario.jpg'); background-size: cover; height: 250px;">
             <div class="text-center" style="background-color: rgba(0, 0, 0, 0.5); padding: 20px; height: 100%;">
                 <h1 class="text-white">¿Quieres registrar tu restaurante?</h1>
-                <?= Html::a('Registrate como propietario', [Url::to('registrar-propietario')], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Registrate como propietario', [Url::to('registrogp')], ['class' => 'btn btn-primary']) ?>
             </div>
         </div>
 
