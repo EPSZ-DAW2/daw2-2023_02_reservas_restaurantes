b6a793ffacb9b29a9cfba26cad133c53a6b29de4
RaulPMusal <raul_pm@usal.es>
2024-01-21 21:39:13 +0100

    sesion bloqueada

--- basic/config/params.php
+++ basic/config/params.php
@@ -4,4 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'maxLoginAttempts' => 4,  // Número máximo de intentos permitidos
+    'loginBlockTime' => 10,  // Tiempo de bloqueo en segundos
 ];
