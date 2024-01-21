51eb6d42cc795dffb8d796924e3a2dc22b24189f
RaulPMusal <raul_pm@usal.es>
2024-01-21 18:58:44 +0100

    busquedas

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -10,9 +10,11 @@ use yii\filters\VerbFilter;
 use app\models\RegistroForm;
 use app\models\LoginForm;
 use app\models\BusquedaFiltrada;
+use app\models\TipoComida;
+use app\models\Restaurante;
 use app\models\ContactForm;
 use app\models\Categoria;
-use app\models\TipoComida;
+use yii\data\ActiveDataProvider;
 
 class SiteController extends Controller
 {
@@ -140,20 +142,95 @@ class SiteController extends Controller
     {
         // creamos el modelo de busqueda
         $model = new BusquedaFiltrada();
-        // cargamos los datos del formulario de filtro si los hay y si se cumple la validacion del registro
-        if ($model->load(Yii::$app->request->post()) && $model->busquedaFiltrada()) { 
-            return $this->goBack();
-        }
 
         //obtenemos las categorias y tipos disponibles de restaurantes en la app
         $categoriasConSubcategorias = Categoria::obtenerCategoriasConPadre();
         $tiposConSubtipos = TipoComida::obtenerTiposConPadre();
 
-        // si no, volvemos a la vista registro con los datos del modelo
+        //obtenemos las comunidadesautonomas, ciudades y barrios de los distintos restaurantes
+        $comunidades = Restaurante::getAllComunidadesAutonomas();
+        $ciudades = Restaurante::getAllCiudades();
+        $barrios = Restaurante::getAllBarrios();
+
+        //obtenemos los IDs de todos los restaurantes para el mostrado sin fltros
+        $IDs = Restaurante::getAllIDs();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => Restaurante::find()->where(['id_restaurante' => $IDs]),
+            'pagination' => [
+                'pageSize' => 8, // Número de elementos por página
+            ],
+        ]);
+
+
+        
+        // cargamos los datos introducidos en la barra de busqueda
+        if (Yii::$app->request->get('palabrasClave')) { 
+            
+            $palabrasClave = Yii::$app->request->get('palabrasClave');            
+
+            //caragmos los ids de la respuesta en caso de haberla
+            $ids_restaurante = $model->busquedaBarra($palabrasClave);
+
+            //si ha encontrado algun restaurante con lo dado en la busqueda
+            if($ids_restaurante != false)
+            {
+                $dataProvider->query->andWhere(['id_restaurante' => $ids_restaurante]);
+            }
+            // si no
+            else
+            {
+                Yii::$app->session->setFlash('warning', 'No se han encontrado restaurantes que cumplan tus filtros de búsqueda. Prueba con otros.');
+
+                return $this->render('busqueda-filtrada', [
+                    'model' => $model,
+                    'categoriasBD' => $categoriasConSubcategorias,
+                    'tiposBD' => $tiposConSubtipos,
+                    'comunidades' => $comunidades,
+                    'ciudades' => $ciudades,
+                    'barrios' => $barrios,
+                    'dataProvider' => null,
+                ]);
+            } 
+        }
+
+
+        // cargamos los datos del formulario de filtro si los hay
+        if ($model->load(Yii::$app->request->post())) { 
+            //caragmos los ids de la respuesta en caso de haberla
+            $ids_restaurante = $model->busquedaFiltrada();
+
+            //si ha ido bien la validacion de datos y ha encontrado algun restaurante con el filtro
+            if($ids_restaurante != false)
+            {
+                $dataProvider->query->andWhere(['id_restaurante' => $ids_restaurante]);
+            }
+            // si no
+            else
+            {
+                Yii::$app->session->setFlash('warning', 'No se han encontrado restaurantes que cumplan tus filtros de búsqueda. Prueba con otros.');
+
+                return $this->render('busqueda-filtrada', [
+                    'model' => $model,
+                    'categoriasBD' => $categoriasConSubcategorias,
+                    'tiposBD' => $tiposConSubtipos,
+                    'comunidades' => $comunidades,
+                    'ciudades' => $ciudades,
+                    'barrios' => $barrios,
+                    'dataProvider' => null,
+                ]);
+            } 
+        }
+
+
         return $this->render('busqueda-filtrada', [
             'model' => $model,
             'categoriasBD' => $categoriasConSubcategorias,
             'tiposBD' => $tiposConSubtipos,
+            'comunidades' => $comunidades,
+            'ciudades' => $ciudades,
+            'barrios' => $barrios,
+            'dataProvider' => $dataProvider,
         ]);
     }
 
